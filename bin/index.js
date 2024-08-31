ac03c29e376424734d26859e5d0b0e0d5a6abe36
arzhes <arzhes@DESKTOP-17EFKQT>
2024-08-31 22:02:44 +0700

    fix input data

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 const fs = require('fs');
-const timers = require('node:timers');
 const readline = require('readline');
 const path = require('path');
 const { timeStamp } = require('console');
@@ -14,73 +13,45 @@ const input = readline.createInterface({
 	output : process.stdout
 });
 
+program
+	.version('1.0.0')
+	.command('task')
+	.description('Add a task')
 
-/**
- * Membaca file notes.json dan menambahkan catatan ke dalamnya.
- * Jika file notes.json tidak ada, maka akan dibuatkan.
- * Jika file notes.json ada maka akan di parse dan di tambahkan notes
- * Jika terjadi error maka akan di tampilkan ke layar.
- */
-function saveNote(notes) {
-	// cek apakah file sudah 
-
-	fs.readFile(filePath, 'utf8', (err, notes) => {
-		// cek apakah terjadi error
-		if (err && err.code !== 'ENOENT') {
-			console.error('Gagal Membaca File', err);
-			return;
-		}
-		let data = [];
-		if (!err && notes) {
-			// parsing
-			try {
-				data = JSON.parse(notes);
-			} catch (parseErr) {
-				console.error('Gagal memparsing file json: ', parseErr);
-				return;
-			}	
-		}
-		data.push(notes);
-		let jsonStrings;
-		try {
-			jsonStrings = JSON.stringify(data,null,2);
-		}	catch(stringifyErr) {
-				console.error('Error mengonversi catatan ke JSON : ', stringifyErr);
-				return;
-			}
-
-	//tambahkan catatan pada notes 
-		fs.writeFile(filePath, jsonStrings, (err) => {
-				if (err) {
-					console.log("Gagal Menyimpan Catatan", err);
-				} 	else {
-					console.log('Catatan berhasil disimpan');
+program
+	.option('-a, --add <options>', 'add a task',(options) => {
+		let opsi = JSON.stringify(options);
+		// cek isi file
+		if (fs.existsSync(filePath)) {
+			let data = [];
+			fs.readFile(filePath, 'utf-8',(err, previousData) => {
+				if (!err && previousData) {
+					console.log(previousData);
+					try {
+						data = JSON.parse(previousData);
+					} catch (error) {
+						console.log(error);
+						return;
+					}
 				}
+			})
 
-				input.close();
-	});
-});
-}
-
-const args = process.argv.slice(2);
-
-if (args.length > 0) {
-	const command = args[0];
-
-	const noteText = args.slice(1).join('');
-	if (command === 'add')
-		if (noteText) {
-
-			saveNote({
-				text : noteText,
-				timestamp : new Date().toISOString()
-			});
 		} else {
-			console.log('Harap masukkan catatan setelah perintah add');
+			fs.writeFileSync(filePath, '[]');
 		}
-	else {
-		console.log("harus menambahkan add");
-	}
-} else {
-	console.log("minimal banget pake 'add'");
-}
+
+		data = fs.readFileSync(filePath, 'utf8');
+		let notes = JSON.parse(opsi);
+		fs.writeFileSync(filePath, JSON.stringify(notes));
+		console.log(`New task added :  ${(options)} !`);
+	})
+	.option('-d, --done', 'done a task')
+	.option('-l, --list', 'list all tasks', () => {
+		let data = fs.readFileSync(filePath, 'utf8');
+		let notes = JSON.parse(data);
+		console.log(notes);
+	})
+	.option('-u, --update', 'update a task');
+
+program.parse(process.argv);
+input.close();
